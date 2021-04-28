d1b028a061c8f3154bd011b826ee49287955fa5e
Erick Castille <ecastille924@gmail.com>
2021-04-28 18:30:04 -0400

    adding edit and delete buttons below each quote

--- index.js
+++ index.js
@@ -14,25 +14,38 @@ fetch("http://localhost:3000/quotes")
         const quotes = quotesResp.data
         quotes.forEach(quotes => {
             const li = document.createElement('li')
+            
+            const editBtn = document.createElement('BUTTON');
+            editBtn.innerText = "Edit"
+            editBtn.addEventListener("click", function(){
+            li.contentEditable = true;
+            })
+
+            const deleteBtn = document.createElement('BUTTON');
+            deleteBtn.innerText = "Delete"
+            deleteBtn.addEventListener("click", function(){
+            })
+
+
             if(quotes.relationships.character.data.id == 1){
                 li.innerText = quotes.attributes.content
-                bobQuotes.append(li)
+                bobQuotes.append(li, editBtn, deleteBtn)
             }
             else if(quotes.relationships.character.data.id == 2){
                 li.innerText = quotes.attributes.content
-                lindaQuotes.append(li)
+                lindaQuotes.append(li, editBtn, deleteBtn)
             }
             else if(quotes.relationships.character.data.id == 3){
                 li.innerText = quotes.attributes.content
-                tinaQuotes.append(li)
+                tinaQuotes.append(li, editBtn, deleteBtn)
             }
             else if(quotes.relationships.character.data.id == 4){
                 li.innerText = quotes.attributes.content
-                geneQuotes.append(li)
+                geneQuotes.append(li, editBtn, deleteBtn)
             }
             else if(quotes.relationships.character.data.id == 5){
                 li.innerText = quotes.attributes.content
-                louiseQuotes.append(li)
+                louiseQuotes.append(li, editBtn, deleteBtn)
             }
         })
     }
